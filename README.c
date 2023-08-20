8bedcdc14ca3147e5a42fe6162867cc0a5b78952
wigleyman <68357577+wigleyman@users.noreply.github.com>
2023-08-20 14:22:44 +0700

    Update and rename README.md to README.c
    First GitHub program. A little about me

---
+++ README.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, world!\nMy name is Martin. I am originally from the UK but live in Asia.\nI have recently developed an   
+obsession with all things programming.\nLearning new stuff every day. It is one of the most challenging and rewarding things I have ever done.\nI love it!");
+}
+
+
--- README.md
+++
@@ -1,5 +0,0 @@
-# hello-world
-First GitHub project
-My name is Martin. I am originally from the UK but live in Asia. 
-I have recently developed an obsession with all things programming.
-Learning new stuff every day. Love it.
